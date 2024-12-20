4c8841bce76a44780c4f331cc9f32a2e417ff498
김도형 <108233361+ShapeKim98@users.noreply.github.com>
2024-12-20 15:32:32 +0900

    dohyeong: 백준 14467 주석 추가

--- week1/dohyeong/백준 14467 소가 길을 건너간 이유 1.swift
+++ week1/dohyeong/백준 14467 소가 길을 건너간 이유 1.swift
@@ -7,20 +7,25 @@
 
 import Foundation
 
+/// - 소와 소 위치 캐싱 초기화
 var cows = [Int: Int]()
+/// - 건너간 소 카운팅 초기화
 var count = 0
 var n = readInt()
 
 for _ in 0..<n {
     let (cow, position) = readTupleInt()
+    /// - 번호에 맞는 소가 존재하고 새로운 위치와 기존의 위치가 다를경우
     if let cowPosition = cows[cow],
        position != cowPosition {
         count += 1
     }
+    /// - 소 위치 업데이트
     cows[cow] = position
 }
 print(count)
 
+/// - 입력받은 소 번호와 소 위치를 튜플로 변환
 func readTupleInt() -> (Int, Int) {
     guard
         let strings = readLine()?.split(separator: " ").map({ String($0) }),
@@ -30,6 +35,7 @@ func readTupleInt() -> (Int, Int) {
     return (int1, int2)
 }
 
+/// - 입력값 정수 변환
 func readInt() -> Int {
     guard
         let sting = readLine(),
